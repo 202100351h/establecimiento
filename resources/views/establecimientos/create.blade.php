29a6727a0180ebb53ee078b9151f68b71f455e0f
Jhojan <202100351h@utea.edu.pe>
2024-04-18 08:40:45 -0500

    estoymuerto

--- resources/views/establecimientos/create.blade.php
+++ resources/views/establecimientos/create.blade.php
@@ -29,8 +29,25 @@
                         {{ $message }}
                     </div>
                     @enderror
+                    <div class="form-group">
+                        <label for="categoria">Categoria</label>
+ 
+                    </div>
+                    <select class="form-control @error('categoria_id"') is-invalid @enderror"
+                                name="categoria_id"
+                                id="categoria">
+                        
+                            <option value="" selected disabled>-- Seleccione --</option>
+
+                            @foreach ($categorias as $categoria)
+                                <option value="{{$categoria -> id}}"
+
+                                {{ old('categoria_id') == $categoria -> id ? 'selected': ''}}
+                                
+                                >{{$categoria->nombre}}</option>
+                            @endforeach
+                        </select>
                 </div>
-                {{$categorias}}
             </fieldset>
         </form>
     </div>
